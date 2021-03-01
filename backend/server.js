a28f7199fd4018d817e04588a3e8291a2de3e0d6
Kh Barsha Singha <khbarshasingha@gmail.com>
2021-03-02 04:29:30 +0530

    error middleware created

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import colors from "colors";
 import dBConnect from "./config/db.js";
 import products from "../backend/products.js";
 import productRoutes from "./Routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js ";
+
 dotenv.config();
 dBConnect();
 const PORT = process.env.PORT;
@@ -15,12 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
-app.use((err, req, res, next) => {
-  const error = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack
-  });
-});
+app.use(notFound);
+
+app.use(errorHandler);
 app.listen(PORT, console.log("server running on port 5000".yellow));
